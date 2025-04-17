3a8be5e6686f049a1028279736099e982cf60bc7
Kirill Salykin <kirill.salykin@gmail.com>
2025-04-17 11:43:57 +0200

    Working example

---
+++ distilled/src/lib.rs
@@ -0,0 +1,83 @@
+#[cfg(feature = "derive")]
+pub use distilled_derive::Distilled;
+
+use serde::{Deserialize, Serialize};
+// use serde_derive::{Deserialize, Serialize};
+use serde_json::Value;
+use std::borrow::Cow;
+use std::collections::BTreeMap;
+use std::collections::HashMap;
+
+pub trait Distilled: Sized {
+    fn distill<'a, T: Into<Option<&'a Value>>>(value: T) -> Result<Self, crate::Error>;
+}
+
+impl Distilled for String {
+    fn distill<'a, T: Into<Option<&'a Value>>>(value: T) -> Result<Self, Error> {
+        let value = value.into().ok_or(Error::entry("missing_field"))?;
+        value
+            .as_str()
+            .map(String::from)
+            .ok_or(Error::entry("wrong_type"))
+    }
+}
+
+impl Distilled for u32 {
+    fn distill<'a, T: Into<Option<&'a Value>>>(value: T) -> Result<Self, Error> {
+        let value = value.into().ok_or(Error::entry("missing_field"))?;
+        let n = value.as_i64().ok_or(Error::entry("wrong_type"))?;
+        u32::try_from(n).map_err(|_| Error::entry("wrong_type"))
+    }
+}
+
+impl<T: Distilled> Distilled for Option<T> {
+    fn distill<'a, U: Into<Option<&'a Value>>>(value: U) -> Result<Self, Error> {
+        match value.into() {
+            Some(v) => Ok(Some(T::distill(v)?)),
+            None => Ok(None),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct ErrorEntry {
+    pub code: Cow<'static, str>,
+    // pub message: Option<Cow<'static, str>>,
+    pub params: HashMap<Cow<'static, str>, Value>,
+}
+
+impl ErrorEntry {
+    pub fn new(code: &'static str) -> Self {
+        Self {
+            code: Cow::from(code),
+            // message: None,
+            params: HashMap::new(),
+        }
+    }
+}
+
+// impl std::error::Error for Error {
+//     fn description(&self) -> &str {
+//         &self.code
+//     }
+//     fn cause(&self) -> Option<&dyn std::error::Error> {
+//         None
+//     }
+// }
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum Error {
+    Struct(HashMap<Cow<'static, str>, Error>),
+    List(BTreeMap<usize, Box<Error>>),
+    Entry(ErrorEntry),
+}
+
+impl Error {
+    pub fn entry(code: &'static str) -> Self {
+        Error::Entry(ErrorEntry::new(code))
+    }
+}
+
+#[derive(Default, Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct ErrorMap(pub HashMap<Cow<'static, str>, Error>);
--- distilled_derive/src/lib.rs
+++ distilled_derive/src/lib.rs
@@ -1,120 +1,96 @@
+// extern crate distilled;
 extern crate proc_macro2;
 
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::Data;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Distilled)]
 pub fn distill_derive(item: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(item as syn::DeriveInput);
+    let input = syn::parse_macro_input!(item as DeriveInput);
+    let ident = &input.ident;
+    let expanded = match &input.data {
+        // Named‐field structs → pull out `named` directly
+        Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+            ..
+        }) => derive_named(ident, named),
 
-    let struct_ident = &input.ident;
+        // Newtypes (1‑field tuple structs)
+        Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Unnamed(syn::FieldsUnnamed { unnamed, .. }),
+            ..
+        }) if unnamed.len() == 1 => {
+            let ty = &unnamed.first().unwrap().ty;
+            derive_newtype(ident, ty)
+        }
 
-    match &input.data {
-        Data::Struct(syn::DataStruct { fields, .. }) => {
-            let num_fields = fields.len();
+        _ => unimplemented!("`Distilled` only supports named structs and newtypes"),
+    };
 
-            let distill_fields = fields.iter().map(|field| {
-              let ident = field.ident.as_ref().unwrap();
-              let ty = &field.ty;
-              let name_str = ident.to_string();
-              quote! {
-                let #ident = <#ty>::distill_from(value.get(#name_str));
-              }
-            });
+    expanded.into()
+}
 
-            let no_errors = fields.iter().map(|field| {
-              let ident = field.ident.as_ref().unwrap();
-              quote! {
-                #ident.is_ok()
-              }
-            });
+fn derive_named(
+    struct_ident: &syn::Ident,
+    fields: &syn::punctuated::Punctuated<syn::Field, syn::token::Comma>,
+) -> proc_macro2::TokenStream {
+    let num_fields = fields.len();
+    let distillers = fields.iter().map(|f| {
+        let name = f.ident.as_ref().unwrap().to_string();
+        let ident = f.ident.as_ref().unwrap();
+        let ty = &f.ty;
+        quote! { let #ident = <#ty>::distill(value.get(#name)); }
+    });
+    let checks = fields.iter().map(|f| {
+        let ident = f.ident.as_ref().unwrap();
+        quote! { #ident.is_ok() }
+    });
+    let unpack = fields.iter().map(|f| {
+        let ident = f.ident.as_ref().unwrap();
+        quote! { #ident: #ident.unwrap() }
+    });
+    let collect_errors = fields.iter().map(|f| {
+        let ident = f.ident.as_ref().unwrap();
+        let name = ident.to_string();
+        quote! {
+          if #ident.is_err() {
+            errors.insert(#name.into(), #ident.err().unwrap());
+          }
+        }
+    });
 
-            let list_fields = fields.iter().map(|field| {
-              let ident = field.ident.as_ref().unwrap();
-              quote! {
-                #ident: #ident.unwrap()
-              }
-            });
+    quote! {
+        impl ::distilled::Distilled for #struct_ident {
+            fn distill<'a, T: Into<Option<&'a serde_json::Value>>>(
+                value: T
+            ) -> Result<Self, ::distilled::Error> {
+                let value = value.into().ok_or_else(|| Error::entry("missing_field"))?;
+                let mut errors = std::collections::HashMap::with_capacity(#num_fields);
 
-            let collect_errors = fields.iter().map(|field| {
-              let ident = field.ident.as_ref().unwrap();
-              let name_str = ident.to_string();
-              quote! {
-                if #ident.is_err() {
-                  errors.insert(#name_str.into(), #ident.err().unwrap());
-                }
-              }
-            });
+                #(#distillers)*
 
-            quote! {
-                impl Distilled for #struct_ident {
-                    fn distill_from<'a, T:std::convert::Into<std::option::Option<&'a serde_json::Value>>>(value: T) -> Result<Self, Error> {
-                        let value = value.into().ok_or(Error::entry("missing_field"))?;
-
-                        let mut errors = std::collections::HashMap::with_capacity(#num_fields);
-
-                        #(#distill_fields)*
-
-                        if #(#no_errors)&&* {
-                          Ok(#struct_ident {
-                           #(#list_fields),*
-                          })
-                        } else {
-                          #(#collect_errors)*
-                          Err(Error::Struct(errors))
-                        }
-                    }
+                if #(#checks)&&* {
+                    Ok(#struct_ident { #(#unpack),* })
+                } else {
+                    #(#collect_errors)*
+                    Err(Error::Struct(errors))
                 }
             }
         }
-        _ => unimplemented!()
-    }.into()
+    }
 }
 
-// struct SignUpInput {
-//     field_string: String,
-//     field_option: Option<u32>,
-//     email: Email,
-//     password: PlainTextPassword,
-// }
-//
-// impl Distilled for SignUpInput {
-//     fn distill_from<'a, T: Into<Option<&'a Value>>>(value: T) -> Result<Self, Error> {
-//         let value = value.into().ok_or(Error::entry("missing_field"))?;
-//
-//         let mut errors = HashMap::with_capacity(4);
-//
-//         let field_string = String::distill_from(value.get("field_string"));
-//         let field_option = Option::<u32>::distill_from(value.get("field_option"));
-//         let email = Email::distill_from(value.get("email"));
-//         let password = PlainTextPassword::distill_from(value.get("password"));
-//
-//         if field_string.is_ok() && field_option.is_ok() && email.is_ok() && password.is_ok() {
-//             Ok(SignUpInput {
-//                 field_string: field_string.unwrap(),
-//                 field_option: field_option.unwrap(),
-//                 email: email.unwrap(),
-//                 password: password.unwrap(),
-//             })
-//         } else {
-//             if field_string.is_err() {
-//                 errors.insert("field_string".into(), field_string.err().unwrap());
-//             }
-//
-//             if field_option.is_err() {
-//                 errors.insert("field_option".into(), field_option.err().unwrap());
-//             }
-//
-//             if email.is_err() {
-//                 errors.insert("email".into(), email.err().unwrap());
-//             }
-//
-//             if password.is_err() {
-//                 errors.insert("password".into(), password.err().unwrap());
-//             }
-//
-//             Err(Error::Struct(errors))
-//         }
-//     }
-// }
+fn derive_newtype(struct_ident: &syn::Ident, inner_ty: &syn::Type) -> proc_macro2::TokenStream {
+    quote! {
+        impl ::distilled::Distilled for #struct_ident {
+            fn distill<'a, T: Into<Option<&'a serde_json::Value>>>(
+                value: T
+            ) -> Result<Self, ::distilled::Error> {
+                let inner = <#inner_ty>::distill(value)?;
+                Ok(#struct_ident(inner))
+            }
+        }
+    }
+}
